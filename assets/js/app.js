22cb402c2e2989aebdf5ac500d43fc459c2540cb
byanezzz <biancayanez89@gmail.com>
2018-02-07 18:30:00 -0300

    trazado de ruta y cambio de icono del marker

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,100 @@
 var divMap = document.getElementById('map');
-navigator.geolocation.getCurrentPosition(success, error);
+var gMap;
+var gMarker;
+initialize();
 
-function error() {};
-
-function success(position) {
-  var lat = position.coords.latitude;
-  var lon = position.coords.longitude;
-
-  var gLatLon = new google.maps.LatLng(lat, lon);
-  var objConf = {
-    zoom: 17,
-    center: gLatLon
-  };
-  var gMap = new google.maps.Map(divMap, objConf);
-  var objConfMarker = {
-    position: gLatLon,
-    map: gMap
+function initialize() {
+  var mapOptions = {
+    center: new google.maps.LatLng(-34.397, 150, 644),
+    zoom: 8
   }
-  var gMarker = new google.maps.Marker(objConfMarker);
-  gMarker.setIcon('assets/img/icono-bici.png');
-};
+  gMap = new google.maps.Map(divMap, mapOptions);
+  var pos = {};
+  // Try HTML5 geolocation.
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = position.coords.latitude;
+      var lon = position.coords.longitude;
+
+      var gLatLon = new google.maps.LatLng(lat, lon);
+      gMap.setZoom(17);
+      gMap.setCenter(gLatLon);
+
+      var objConfMarker = {
+        position: gLatLon,
+        map: gMap
+      }
+      gMarker = new google.maps.Marker(objConfMarker);
+      gMarker.setIcon('assets/img/icono-bici.png');
+    }, function() {
+      alert('error');
+    });
+  } else {
+    alert('error');
+  }
+}
+var start;
+var stop;
+$('#origin').change(function() {
+
+  var gCoderOrigin = new google.maps.Geocoder();
+  var objInformationOrigin = {
+    address: $('#origin').val()
+  };
+  gCoderOrigin.geocode(objInformationOrigin, coderOrigin);
+
+  function coderOrigin(data) {
+    star = data[0].geometry.location; //Lo mismo que el objeto LatLong
+  };
+});
+$('#final').change(function() {
+
+  var gCoderFinal = new google.maps.Geocoder();
+  var objInformationFinal = {
+    address: $('#final').val()
+  };
+  gCoderFinal.geocode(objInformationFinal, coderFinal);
+
+  function coderFinal(data) {
+    stop = data[0].geometry.location; //Lo mismo que el objeto LatLong    
+  }
+
+});
+$('#route').click(function() {
+  gMarker.setMap(null);
+  var objConfigDR = {
+    map: gMap,
+    suppressMarkers: true
+  }
+  var startMarker = new google.maps.Marker({
+    position: star,
+    map: gMap,
+    icon: 'assets/img/icono-bici.png'
+  });
+  var stopMarker = new google.maps.Marker({
+    position: stop,
+    map: gMap,
+    icon: 'assets/img/icono-bici.png'
+  });
+
+  var objConfigDS = {
+    origin: star,
+    destination: stop,
+    travelMode: google.maps.TravelMode.WALKING
+  }
+  var ds = new google.maps.DirectionsService();
+  var dr = new google.maps.DirectionsRenderer(objConfigDR);
+  ds.route(objConfigDS, getRoute);
+
+  function getRoute(dataRoute, status) {
+    if (status === 'OK') {
+      dr.setDirections(dataRoute);
+    } else {
+      alert('No hay ruta establecida en Bici para esta dirección ');
+    }
+  }
+});
 // Autocompletar Input
 var defaultBounds = new google.maps.LatLngBounds(
   new google.maps.LatLng(-33.8902, 151.1759),
@@ -32,37 +107,3 @@ var inputOrigin = document.getElementById('origin');
 var autocompleteOrigin = new google.maps.places.Autocomplete(inputOrigin, options);
 var inputFinal = document.getElementById('final');
 var autocompleteFinal = new google.maps.places.Autocomplete(inputFinal, options);
-/* 
-  var gCoder = new google.maps.Geocoder();
-  var objInformation = {
-    address: 'Eleuterio Ramírez 1027,Santiago,Santiago de Chile'
-  };
-  gCoder.geocode(objInformation, coder)
-
-  function coder(data) {
-    var coord = data[0].geometry.location; //Lo mismo que el objeto LatLong
-    var confMarker = {
-      position: coord,
-      map: gMap
-    };
-    var gMarkerDV = new google.maps.Marker(confMarker);
-  }
-  var objConfigDR = {
-    map: gMap
-  }
-  var objConfigDS = {
-    origin: gLatLon,
-    destination: objInformation.address,
-    travelMode: google.maps.TravelMode.DRIVING
-  }
-  var ds = new google.maps.DirectionsService();
-  var dr = new google.maps.DirectionsRenderer(objConfigDR);
-  ds.route(objConfigDS, getRoute);
-
-  function getRoute(dataRoute, status) {
-    if (status === 'OK') {
-      dr.setDirections(dataRoute);
-    } else {
-      alert('No hay ruta establecida en Bici para esta dirección ');
-    }
-  } */
